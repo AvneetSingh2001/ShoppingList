701d97f8bf05130636ac0d218bb222875702a736
Avneet <avneetmaankiya@gmail.com>
2021-10-13 23:50:02 +0530

    ViewModel

---
+++ app/src/main/java/com/example/shoppinglist/ui/Shoppinglist/ShoppingViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.shoppinglist.ui.Shoppinglist
+
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.db.entities.ShoppingItem
+import com.example.shoppinglist.data.repositories.ShoppingRepositories
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ShoppingViewModel(
+    private val Repository: ShoppingRepositories
+): ViewModel() {
+    fun upsert(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
+        Repository.upsert(item)
+    }
+
+    fun delete(item: ShoppingItem) = CoroutineScope(Dispatchers.Main).launch {
+        Repository.delete(item)
+    }
+
+    fun getAllShoppingItems() = Repository.getAllShoppingItems()
+
+}
